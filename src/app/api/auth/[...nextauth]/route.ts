77701f08b0f758087834060f7f2a50be99ac1597
shogun444 <sayandiproy17@gmail.com>
2025-05-12 00:48:11 +0530

    Authentication Done

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,12 +1,31 @@
 import NextAuth from "next-auth"
 import GoogleProvider from "next-auth/providers/google";
+import { prisma } from "../../../../../prisma/prisma";
 const handler = NextAuth({
   providers: [
   GoogleProvider({
     clientId: process.env.GOOGLE_CLIENT_ID as string,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET as string
   })
-]
+],
+callbacks : {
+  async signIn({user,account}){
+    if(user && account) { 
+    
+  await prisma.user.create({
+    data:{
+      email : user.email as string,
+      name : user.name as string,
+      provider : account?.provider 
+    }
+  })
+ 
+    }
+
+  
+return true
+  }
+}
 })
 
 export { handler as GET, handler as POST }
--- src/app/api/songs/route.ts
+++ src/app/api/songs/route.ts
@@ -1,22 +1,25 @@
 import { NextRequest, NextResponse } from "next/server";
 import { prisma } from "../../../../prisma/prisma";
-import {string, z} from 'zod'
+import {z} from 'zod'
 import axios from "axios";
 
 export async function GET(req : NextRequest){
 
-const AllSongs = await prisma.song.findMany({})
+const AllSongs = await prisma.song.findMany( {} )
 return NextResponse.json({msg : "All current Songs",AllSongs},{status:200})
 }
 
 export async function POST(req : NextRequest){
 const validYtlink = /^(https?:\/\/)?(www\.)?youtube\.com\/watch\?v=[\w-]{11}(&[\w=&-]*)?$/
-const LinkSchema = z.string().min(1,{message : `Link can't be empty`})
+const DataSchema = z.object({
+  link :  z.string().min(1,{message : `Link can't be empty`}),
+  userId : z.string().min(1,{message : `Can't be empty`})
+}) 
 
 
 try {
-  const {link} = await req.json();
-  const ValidateLink =  LinkSchema.safeParse(link)
+  const {link,userId} = await req.json();
+  const ValidateLink =  DataSchema.safeParse({link,userId})
   if(!ValidateLink.success){
      return NextResponse.json({msg : 'Link is Empty'},{status : 400})
   }
@@ -27,17 +30,28 @@ try {
  const videoId = link.split('?')[1]
 
  const res = await axios.get(`https://www.youtube.com/oembed?url=https://www.youtube.com/watch?${videoId}&format=json`)
+
+
  if(res){
   const existingData = await prisma.song.findFirst({
     where : {
-      link : link
+      link : link,
+      userId : userId  
     }
   })
   if(existingData){
     return NextResponse.json({msg : "The Link has already been Added"},{status : 400})
   }
+
+  const checkUser = await prisma.user.findUnique({
+    where : {
+      id :userId
+    }
+  })
+  if(checkUser) { 
 const Song = await prisma.song.create({
   data : {
+    userId : checkUser.id,
     link : link , 
     videoId : videoId,
     name : res.data.author_name,
@@ -46,6 +60,8 @@ const Song = await prisma.song.create({
   }
 })
 return NextResponse.json({msg : "Link Created Succsfully",Song},{status : 200})
+  }
+
  } 
  else return NextResponse.json({msg : "Can't find data with the link provided"},{status:404})
  }
