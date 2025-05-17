868bf3a71f0bc47b354acf7666118b41e90de15a
shogun444 <sayandiproy17@gmail.com>
2025-05-17 19:13:26 +0530

    added videos

--- src/app/components/Landing.tsx
+++ src/app/components/Landing.tsx
@@ -1,3 +1,7 @@
+import { Button } from "@/components/ui/button"
+
+
+
 export default function Landing(){
   const videos = [{
     src : '/Piano.mp4'
@@ -6,29 +10,35 @@ export default function Landing(){
     src : '/drums.mp4'
   },
 {
-    src : '/Production.mp4'
+    src : '/Guitar1.mp4'
   },
 {
-    src : '/Singing.mp4'
+    src : '/Guitar2.mp4'
   },
 {
-    src : '/AcousticGuitar.mp4'
+    src : '/Synth.mp4'
+  },
+  {
+    src : '/Vocals.mp4'
   }
 ]
   return(<>
   <div
   className="min-h-screen  w-full bg-neutral-900">
-<div className="flex justify-center items-center pr-10">
-  <div className="max-w-6xl pr-50">
-    <h1 className="text-5xl  font-semibold text-neutral-300">Your Music, Reimagined</h1>
-    <p className="text-neutral-600 font-semibold">Experience music like never before with our premium audio platform. 
+<div className="flex justify-center items-center ">
+  <div className="max-w-6xl pr-5 ">
+    <h1 className="text-6xl  font-semibold text-neutral-200">Your Music, Reimagined</h1>
+    <p className="text-neutral-400 pt-5 font-semibold">Experience music like never before with  our premium audio platform. 
       <br />
-      Discover, stream, and share your favorite tracks with unparalleled sound quality.</p>
+      Discover, stream, and share your favorite tracks with unparalleled  <br />sound quality.</p>
+
+      <Button size={'lg'} className="bg-cyan-700 text-neutral-900 text-md p-6 mt-3">Start Free</Button>
     </div>
 
-    <div className="grid grid-cols-2 w-fit gap-2  p-5">
-      {videos.map((itm,index)=>( <div key={index} className="">
-        <video className="rounded-lg" width={100} height={50} autoPlay loop muted src={itm.src}></video>
+
+    <div className="grid grid-cols-2 w-fit gap-2  p-2">
+      {videos.map((itm,index)=>( <div key={index} className="mask-y-from-98% mask-x-from-97% ">
+        <video className="rounded-lg" width={360} height={100} autoPlay loop muted src={itm.src}></video>
       </div> ))}
       
        </div>
